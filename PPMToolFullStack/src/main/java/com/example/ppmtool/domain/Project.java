c9e06e15cf609561ca4872be2a6b56361d904dd4
David B <admin@localhost.dev>
2019-03-04 19:06:13 +0100

    project service and controller

--- PPMToolFullStack/src/main/java/com/example/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/domain/Project.java
@@ -9,7 +9,7 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String projectname;
+    private String projectName;
     private String projectIdentifier;
     private String description;
     private Date start_date;
@@ -39,12 +39,12 @@ public class Project {
         this.id = id;
     }
 
-    public String getProjectname() {
-        return projectname;
+    public String getProjectName() {
+        return projectName;
     }
 
-    public void setProjectname(String projectname) {
-        this.projectname = projectname;
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
     }
 
     public String getProjectIdentifier() {
