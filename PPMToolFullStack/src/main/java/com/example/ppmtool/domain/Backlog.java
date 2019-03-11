22b3dd4fdcaadaee5e48f0392ce532386d6bd439
David B <admin@localhost.dev>
2019-03-11 19:50:52 +0100

    Add Project Tasks - Backend: Entity OneToOne Relationships - Project and Backlog

--- PPMToolFullStack/src/main/java/com/example/ppmtool/domain/Backlog.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/domain/Backlog.java
@@ -1,9 +1,8 @@
 package com.example.ppmtool.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 
 @Entity
 public class Backlog {
@@ -14,7 +13,11 @@ public class Backlog {
     private Integer PTSequence = 0;
     private String projectIdentifier;
 
-    // OneToOne with project - backlog
+    // OneToOne with project
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "project_id", nullable = false)
+    @JsonIgnore     // break infinity recursion
+    private Project project;
 
     // OneToMany projecttasks
 
@@ -44,5 +47,13 @@ public class Backlog {
 
     public void setProjectIdentifier(String projectIdentifier) {
         this.projectIdentifier = projectIdentifier;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
     }
 }
