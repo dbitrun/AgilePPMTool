b0a3d238fbf1a59d303fb5dcb068c486c9f7da55
David B <admin@localhost.dev>
2019-03-14 19:12:37 +0100

    Add Project Tasks - Backend: Update ProjectTask by projectSequence - creating updateByProjectSequence() in ProjectTaskService and updateProjectTask() in BacklogController

--- PPMToolFullStack/src/main/java/com/example/ppmtool/web/BacklogController.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/web/BacklogController.java
@@ -49,4 +49,19 @@ public class BacklogController {
         return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
 
     }
+
+    @PatchMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> updateProjectTask(
+            @Valid @RequestBody ProjectTask projectTask,
+            BindingResult result,
+            @PathVariable String backlog_id,
+            @PathVariable String pt_id) {
+
+        ResponseEntity<?> errMap = mapValidationErrorService.mapValidationService(result);
+        if (errMap != null) return errMap;
+
+        ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask, backlog_id, pt_id);
+
+        return new ResponseEntity<ProjectTask>(updatedTask, HttpStatus.OK);
+    }
 }
