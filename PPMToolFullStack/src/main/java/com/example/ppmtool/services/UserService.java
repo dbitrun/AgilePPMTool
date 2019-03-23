f6f8ebf46b4198d12f411ca99be0ec82869d9923
David B <admin@localhost.dev>
2019-03-24 00:35:40 +0100

    Spring Security and JWT: User Registration - Creating UsernameAlreadyExistsException and UsernameAlreadyExistsResponse, registering exception in CustomResponseEntityExceptionHandler, using exception in saveUser() in UserService

--- PPMToolFullStack/src/main/java/com/example/ppmtool/services/UserService.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.ppmtool.services;
 
 import com.example.ppmtool.domain.User;
+import com.example.ppmtool.exceptions.UsernameAlreadyExistsException;
 import com.example.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,14 +18,17 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser(User newUser) {
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
-
-        // Username has to be unique (exception)
-
-        // Make sure that password and confirmPassword match
-        // We don't persist or show the confirmPassword
-
-        return userRepository.save(newUser);
+        try {
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            // Username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            // Make sure that password and confirmPassword match
+            // We don't persist or show the confirmPassword
+            return userRepository.save(newUser);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            throw new UsernameAlreadyExistsException("Username '" + newUser.getUsername() + "' already exists");
+        }
     }
 
 }
