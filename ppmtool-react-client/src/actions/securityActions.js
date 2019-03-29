2684196883bf013d507a19160da94ab2b335b2b4
David B <admin@localhost.dev>
2019-03-29 23:11:36 +0100

    Secure React App: Creating securityActions, securityReducer, setJWTToken (in securityUtils), installing npm package jwt-decode

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
   try {
@@ -8,6 +10,32 @@ export const createNewUser = (newUser, history) => async dispatch => {
     dispatch({
       type: GET_ERRORS,
       payload: {}
+    });
+  } catch (error) {
+    console.log(error);
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const login = loginRequest => async dispatch => {
+  try {
+    // post => Login Request
+    const res = await axios.post("/api/users/login", loginRequest);
+    // extract token from res.data
+    const { token } = res.data;
+    // store the token in the localStorage
+    localStorage.setItem("jwtToken", token);
+    // set our token in header ***
+    setJWTToken(token);
+    // decode token on React
+    const decoded = jwt_decode(token);
+    // dispatch to our securityReducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded
     });
   } catch (error) {
     console.log(error);
